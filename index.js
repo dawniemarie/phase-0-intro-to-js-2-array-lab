fb1be553430f535549c7cf0d1642f505ae4a5de7
Dawn Olivier <dmarieolivier@gmail.com>
2022-04-30 21:27:04 -0400

    commit tree b49d529b1081c83851754bd26179b0b822f1585b

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+    cats.push('Ralph')
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat() {
+    const newCats = [...cats, "Broom"]
+    return newCats;
+}
+function prependCat() {
+    const moreCats = ["Arnold", ...cats]
+    return moreCats;
+}
+function removeLastCat() {
+    const removeLastCat = [
+        ...cats.slice(0,-1)
+    ]
+    return removeLastCat;
+}
+function removeFirstCat() {
+    const removeFirstCat = [
+        ...cats.slice(1)
+    ]
+    return removeFirstCat;
+}
